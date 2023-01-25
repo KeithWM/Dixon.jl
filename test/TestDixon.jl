c2e6638172f3d7899e13ee0e4611754cbd876d1c
KeithWM <keith@myerscough.nl>
2023-01-25 14:51:46 +0100

    Include a test to test expansions around other values than zero.

--- test/TestDixon.jl
+++ test/TestDixon.jl
@@ -31,49 +31,70 @@ COEFFICIENTS = Dixon.Coefficients{Float64,Dixon.DixonElliptic}(100, 0, (1, 0))
 cm(x::Number) = compute(x, COEFFICIENTS, Val{:cm}())
 sm(x::Number) = compute(x, COEFFICIENTS, Val{:sm}())
 
+const VALUE_TABLE = Dict{
+    Rational,NamedTuple{(:cm, :sm, :broken),Tuple{Float64,Float64,Bool}}
+}(
+    # -1//3	=> (cm=Inf, sm=Inf, broken=true),
+    -1//6 => (cm=cbrt(2), sm=-1, broken=false),
+    0 => (cm=1, sm=0, broken=false),
+    1//6 => (cm=1 / cbrt(2), sm=1 / cbrt(2), broken=false),
+    1//3 => (cm=0, sm=1, broken=false),
+    1//2 => (cm=-1, sm=cbrt(2), broken=false),
+    # 2//3	=> (cm=Inf, sm=Inf, broken=true),
+    -1//4 => (
+        cm=(1 + sqrt(3) + sqrt(2 * sqrt(3))) / 2,
+        sm=(-1 - sqrt(3 + 2 * sqrt(3))) / cbrt(4),
+        broken=false,
+    ),
+    -1//12 => (
+        cm=(-1 + sqrt(3) + sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
+        sm=(-1 + sqrt(3) - sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
+        broken=false,
+    ),
+    1//12 => (
+        cm=(-1 + sqrt(3 + 2 * sqrt(3))) / cbrt(4),
+        sm=(1 + sqrt(3) - sqrt(2 * sqrt(3))) / 2,
+        broken=false,
+    ),
+    1//4 => (
+        cm=(1 + sqrt(3) - sqrt(2 * sqrt(3))) / 2,
+        sm=(-1 + sqrt(3 + 2 * sqrt(3))) / cbrt(4),
+        broken=false,
+    ),
+    5//12 => (
+        cm=(-1 + sqrt(3) - sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
+        sm=(-1 + sqrt(3) + sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
+        broken=false,
+    ),
+    7//12 => (
+        cm=(-1 - sqrt(3 + 2 * sqrt(3))) / cbrt(4),
+        sm=(1 + sqrt(3) + sqrt(2 * sqrt(3))) / 2,
+        broken=false,
+    ),
+)
+
 @testset "Test specific values" begin
-    table = Dict(
-        # -1//3	=> (cm=Inf, sm=Inf, broken=true),
-        -1//6 => (cm=cbrt(2), sm=-1, broken=false),
-        0 => (cm=1, sm=0, broken=false),
-        1//6 => (cm=1 / cbrt(2), sm=1 / cbrt(2), broken=false),
-        1//3 => (cm=0, sm=1, broken=false),
-        1//2 => (cm=-1, sm=cbrt(2), broken=false),
-        # 2//3	=> (cm=Inf, sm=Inf, broken=true),
-        -1//4 => (
-            cm=(1 + sqrt(3) + sqrt(2 * sqrt(3))) / 2,
-            sm=(-1 - sqrt(3 + 2 * sqrt(3))) / cbrt(4),
-            broken=false,
-        ),
-        -1//12 => (
-            cm=(-1 + sqrt(3) + sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
-            sm=(-1 + sqrt(3) - sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
-            broken=false,
-        ),
-        1//12 => (
-            cm=(-1 + sqrt(3 + 2 * sqrt(3))) / cbrt(4),
-            sm=(1 + sqrt(3) - sqrt(2 * sqrt(3))) / 2,
-            broken=false,
-        ),
-        1//4 => (
-            cm=(1 + sqrt(3) - sqrt(2 * sqrt(3))) / 2,
-            sm=(-1 + sqrt(3 + 2 * sqrt(3))) / cbrt(4),
-            broken=false,
-        ),
-        5//12 => (
-            cm=(-1 + sqrt(3) - sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
-            sm=(-1 + sqrt(3) + sqrt(2 * sqrt(3))) / (2 * cbrt(2)),
-            broken=false,
-        ),
-        7//12 => (
-            cm=(-1 - sqrt(3 + 2 * sqrt(3))) / cbrt(4),
-            sm=(1 + sqrt(3) + sqrt(2 * sqrt(3))) / 2,
-            broken=false,
-        ),
-    )
-    @testset "Value $z * pi3" for (z, expected) in table
+    @testset "Value $z * pi3" for (z, expected) in VALUE_TABLE
         @test isapprox(cm(z * pi3), expected[:cm]; rtol=1.e-10) broken = expected[:broken]
         @test isapprox(sm(z * pi3), expected[:sm]; rtol=1.e-10) broken = expected[:broken]
+    end
+end
+
+@testset "Test local expansion" begin
+    sorted = VALUE_TABLE |> keys |> collect |> sort
+    @testset "Value $i: $z * pi3" for (i, z) in enumerate(sorted[begin:(end - 1)])
+        c_0 = VALUE_TABLE[z][:cm]
+        s_0 = VALUE_TABLE[z][:sm]
+        z_0 = z * pi3
+        c_1 = VALUE_TABLE[sorted[i + 1]][:cm]
+        s_1 = VALUE_TABLE[sorted[i + 1]][:sm]
+        z_1 = sorted[i + 1] * pi3
+        coeff_0 = Dixon.Coefficients{Float64,Dixon.DixonElliptic}(100, z_0, (c_0, s_0))
+        coeff_1 = Dixon.Coefficients{Float64,Dixon.DixonElliptic}(100, z_1, (c_1, s_1))
+        reconstructed_1 = compute(z_1, coeff_0, Val{:cm}())
+        reconstructed_0 = compute(z_0, coeff_1, Val{:cm}())
+        @test isapprox(reconstructed_1, c_1; rtol=1.e-14)
+        # @test isapprox(reconstructed_0, c_0; rtol=1.e-14)
     end
 end
 
